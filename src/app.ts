23effd57ae84bf221d552107ff3fc258df7c51de
sreekanta2 <sreekanta03s@gmail.com>
2024-02-05 01:45:48 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { Application } from "express";
+import express, { Application, Request } from "express";
 const app: Application = express();
 
 app.use(
@@ -13,5 +13,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.get("/", (req: Request, res) => {
+  res.send("Working successfully");
+});
 
 export default app;
